e7f22e5b65e2bc9e0077e18496b8f480cf64f038
unknown <jas1421@GDD-49.main.ad.rit.edu>
2015-04-12 15:45:43 -0400

    Gun shooting works

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	public float time;
+	public bool hitObject;
+	public GameObject pic;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//If the gameobject hasnt hit anything for "time" then destroy
+		Destroy (this.gameObject, time);
+		hitObject = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		//If the bullet collides with something destroy it
+		Destroy (this.gameObject);
+		hitObject = true;
+		//For Instatiating bulletHole texture..
+		//Rigidbody clone = Instantiate(pic, transform.position, transform.rotation) as Rigidbody;
+
+
+	}
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public Rigidbody projectile;
+	float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		speed = 2000;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			//Creates rigidbody object from prefab
+			Rigidbody clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+			//Pushed the object foward with forces
+			clone.AddForce(transform.forward * speed);
+		}
+	}
+
+}
