ddb9ef5711b3d0fe252bb75baebfe8d8e2e2e612
unknown <jas1421@OH1375-13.main.ad.rit.edu>
2015-04-13 09:40:37 -0400

    tried rotating bullet

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -6,20 +6,27 @@ public class Shoot : MonoBehaviour {
 	public Rigidbody projectile;
 	float speed;
 
+	//private Vector3 targetAngles;
+
 	// Use this for initialization
 	void Start () 
 	{
-		speed = 2000;
+		speed = 900;
 
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+
 		if (Input.GetMouseButtonDown (0)) 
 		{
 			//Creates rigidbody object from prefab
 			Rigidbody clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+
+			//targetAngles = transform.eulerAngles + 180f * Vector3.up; // what the new angles should be
+			//clone.transform.eulerAngles = Vector3.Lerp(clone.transform.eulerAngles, targetAngles, Time.deltaTime); // lerp to new angles
+
 			//Pushed the object foward with forces
 			clone.AddForce(transform.forward * speed);
 		}
