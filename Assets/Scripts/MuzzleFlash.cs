8a50c82d0265baf64378059f3518ae2c70945826
unknown <nas2375@GDD-58.main.ad.rit.edu>
2015-04-19 18:26:06 -0400

    fixed bullet direction added muzzle flash texture,material,light,code (need placement)

---
+++ Assets/Scripts/MuzzleFlash.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuzzleFlash : MonoBehaviour {
+
+	float startTime;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0.05f;
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > startTime + timer) {
+			Destroy (this.gameObject);
+		}
+	}
+}
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -8,6 +8,11 @@ public class Shoot : MonoBehaviour {
 	public Camera MainCamera; 
 	public Camera ScopeCamera;
 
+	public GameObject MuzzleFlashObj;
+	float muzzleStart;
+	float muzzleTimer;
+	bool shot;
+
 	//private Vector3 targetAngles;
 
 	// Use this for initialization
@@ -16,23 +21,35 @@ public class Shoot : MonoBehaviour {
 		speed = 900;
 		MainCamera.enabled = true; 
 		ScopeCamera.enabled = false; 
-
+		muzzleTimer = 0.05f;
+		shot = false;
+		MuzzleFlashObj.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		if ((Time.time > muzzleStart + muzzleTimer) && shot) {
+			MuzzleFlashObj.SetActive(false);
+			shot = false;
+		}
 
 		if (Input.GetMouseButtonDown (0)) 
 		{
 			//Creates rigidbody object from prefab
-			Rigidbody clone = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+			Rigidbody clone = Instantiate(projectile, transform.position, this.gameObject.transform.rotation) as Rigidbody;
+			clone.transform.parent = this.transform;
+			clone.transform.localRotation = Quaternion.Euler(90, 0, 0);
 			//clone.transform.RotateAround (Vector3.zero, Vector3.up, 2 * Time.deltaTime);
 			//targetAngles = transform.eulerAngles + 180f * Vector3.up; // what the new angles should be
 			//clone.transform.eulerAngles = Vector3.Lerp(clone.transform.eulerAngles, targetAngles, Time.deltaTime); // lerp to new angles
 
 			//Pushed the object foward with forces
 			clone.AddForce(transform.forward * speed);
+
+			muzzleStart = Time.time;
+			MuzzleFlashObj.SetActive(true);
+			shot = true;
 		}
 		if (Input.GetMouseButtonDown (1)) 
 		{
