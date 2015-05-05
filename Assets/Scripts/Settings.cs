673a7c3c573ed0aaa28939c9bd8a0193b5e5a7c9
unknown <nas2375@GDD-47.main.ad.rit.edu>
2015-05-04 22:33:41 -0400

    added scoreboard added raycasting removed bullet collision

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Settings : MonoBehaviour {
 
+	public Texture2D crosshairTexture;
+
 	// Use this for initialization
 	void Start () {
 		Screen.lockCursor = true;
@@ -11,5 +13,10 @@ public class Settings : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect((Screen.width - crosshairTexture.width) / 2, (Screen.height - crosshairTexture.height) / 2, crosshairTexture.width, crosshairTexture.height), crosshairTexture);
 	}
 }
